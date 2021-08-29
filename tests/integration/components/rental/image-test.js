fb5d194f5e3a9ca62adf6c76d57a9cb1a9bf7072
YeCatherine <Catherine.tatianenko@gmail.com>
2021-08-30 00:16:42 +0300

    added image size if/else switcher

--- tests/integration/components/rental/image-test.js
+++ tests/integration/components/rental/image-test.js
@@ -1,12 +1,12 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import { hbs } from 'ember-cli-htmlbars';
+import {module, test} from 'qunit';
+import {setupRenderingTest} from 'ember-qunit';
+import {render} from '@ember/test-helpers';
+import {hbs} from 'ember-cli-htmlbars';
 
-module('Integration | Component | rental/image', function (hooks) {
+module('Integration | Component | rental/image', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders the given image', async function (assert) {
+  test('it renders the given image', async function(assert) {
     await render(hbs`
       <Rental::Image
         src="/assets/images/teaching-tomster.png"
@@ -14,10 +14,9 @@ module('Integration | Component | rental/image', function (hooks) {
       />
     `);
 
-    assert
-    .dom('.image img')
-    .exists()
-    .hasAttribute('src', '/assets/images/teaching-tomster.png')
-    .hasAttribute('alt', 'Teaching Tomster');
+    assert.dom('.image img').
+      exists().
+      hasAttribute('src', '/assets/images/teaching-tomster.png').
+      hasAttribute('alt', 'Teaching Tomster');
   });
 });
